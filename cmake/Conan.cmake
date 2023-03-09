427a8c7609067dbc8ba30d3962c03fc829bf4f8d
Klaus Schnass <klaus.schnass@zuehlke.com>
2023-03-09 14:49:04 +0100

    update cmake-conan and use new CONF section for specifying desired cmake generator

--- cmake/Conan.cmake
+++ cmake/Conan.cmake
@@ -2,7 +2,7 @@ MACRO(RUN_CONAN)
     # Download automatically, you can also just copy the conan.cmake file
     IF(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
         MESSAGE(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
-        FILE(DOWNLOAD "https://github.com/conan-io/cmake-conan/raw/0.18.1/conan.cmake" "${CMAKE_BINARY_DIR}/conan.cmake" TLS_VERIFY ON)
+        FILE(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/9445e140706012c218bb2283620bec950190359d/conan.cmake" "${CMAKE_BINARY_DIR}/conan.cmake" TLS_VERIFY ON)
     ENDIF()
 
     INCLUDE(${CMAKE_BINARY_DIR}/conan.cmake)
@@ -12,17 +12,21 @@ MACRO(RUN_CONAN)
         SET(FMT_LIBRARY fmt/9.1.0)
     ENDIF()
 
-    CONAN_CMAKE_RUN(
-            REQUIRES
-            ${CONAN_EXTRA_REQUIRES}
-            catch2/3.2.1
-            ${FMT_LIBRARY}
-            OPTIONS
-            ${CONAN_EXTRA_OPTIONS}
-            SETTINGS
-            compiler.cppstd=${CMAKE_CXX_STANDARD}
-            BASIC_SETUP
-            CMAKE_TARGETS # individual targets to link to
-            BUILD
-            missing)
+    conan_cmake_configure(REQUIRES  ${CONAN_EXTRA_REQUIRES}
+                                    catch2/3.2.1
+                                    ${FMT_LIBRARY}
+                          OPTIONS	${CONAN_EXTRA_OPTIONS}
+                          SETTINGS  compiler.cppstd=${CMAKE_CXX_STANDARD}
+                          GENERATORS cmake
+            )
+
+    conan_cmake_autodetect(settings)
+
+    conan_cmake_install(PATH_OR_REFERENCE .
+                        BUILD missing
+                        SETTINGS ${settings}
+                        CONF "tools.cmake.cmaketoolchain:generator=${CMAKE_GENERATOR}")
+
+    include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+    conan_basic_setup(TARGETS)
 ENDMACRO()
