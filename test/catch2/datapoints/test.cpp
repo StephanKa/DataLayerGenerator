00e75fb9e1023f0c58c8962df3f069d727759b3d
Stephan Kantelberg <stephan.kantelberg@zuehlke.com>
2023-03-25 19:03:15 +0100

    use SECTION and setup / teardown

--- test/catch2/datapoints/test.cpp
+++ test/catch2/datapoints/test.cpp
@@ -5,208 +5,223 @@
 namespace {
 constexpr auto EPSILON = 0.1;
 
-constexpr bool operator==(const Temperature &lhs, const Temperature &rhs) { return (lhs.raw == rhs.raw) && (static_cast<double>(std::fabs(lhs.value - rhs.value)) <= EPSILON); }
+constexpr bool operator==(const Temperature &lhs, const Temperature &rhs)
+{
+    return (lhs.raw == rhs.raw) && (static_cast<double>(std::fabs(lhs.value - rhs.value)) <= EPSILON);
+}
 }// namespace
 
-TEST_CASE("Test datapoint default values via get() method", "[Datapoints]")
+TEST_CASE("Test datapoints")
 {
-    constexpr auto EXPECTED = 123.0;
-    REQUIRE(test.get().raw == 4711);
-    REQUIRE_THAT(test.get().value, Catch::Matchers::WithinRel(EXPECTED, EPSILON));
-}
+    const auto initalTestValue = test();
+    const auto initalTestWithoutDefaultValueValue = testWithoutDefaultValue();
+    const auto initalTestWithoutDefaultValueWriteOnlyValue = testWithoutDefaultValueWriteOnly();
 
-TEST_CASE("Test datapoint default values with () operator", "[Datapoints]")
-{
-    constexpr auto EXPECTED = 123.0;
-    REQUIRE(test().raw == 4711);
-    REQUIRE_THAT(test().value, Catch::Matchers::WithinRel(EXPECTED, EPSILON));
-}
+    SECTION("datapoint default values via get() method")
+    {
+        constexpr auto EXPECTED = 123.0;
+        REQUIRE(test.get().raw == 4711);
+        REQUIRE_THAT(test.get().value, Catch::Matchers::WithinRel(EXPECTED, EPSILON));
+    }
 
-TEST_CASE("Test version definition", "[Datapoints]")
-{
-    constexpr auto version = test.getVersion();
-    REQUIRE(version.major == 1);
-    REQUIRE(version.minor == 0);
-    REQUIRE(version.build == 22);
-}
+    SECTION("datapoint default values with () operator")
+    {
+        constexpr auto EXPECTED = 123.0;
+        REQUIRE(test().raw == 4711);
+        REQUIRE_THAT(test().value, Catch::Matchers::WithinRel(EXPECTED, EPSILON));
+    }
 
-TEST_CASE("Test datapoint id definition", "[Datapoints]")
-{
-    constexpr auto expectedDefaultGroupID = 0x4000;
-    constexpr uint16_t expectedDatapointId = 0x4;
-    REQUIRE(DefaultGroupInfo.baseId == expectedDefaultGroupID);
-    REQUIRE(test.getId() == (expectedDefaultGroupID + expectedDatapointId));
-}
+    SECTION("version definition")
+    {
+        constexpr auto version = test.getVersion();
+        REQUIRE(version.major == 1);
+        REQUIRE(version.minor == 0);
+        REQUIRE(version.build == 22);
+    }
 
-TEST_CASE("Test datapoint type", "[Datapoints]")
-{
-    const auto value = test.get();
-    constexpr Temperature expected{};
-    static_assert(std::is_same_v<decltype(value), decltype(expected)>);
-}
+    SECTION("datapoint id definition")
+    {
+        constexpr auto expectedDefaultGroupID = 0x4000;
+        constexpr uint16_t expectedDatapointId = 0x4;
+        REQUIRE(DefaultGroupInfo.baseId == expectedDefaultGroupID);
+        REQUIRE(test.getId() == (expectedDefaultGroupID + expectedDatapointId));
+    }
 
-TEST_CASE("Test datapoint access", "[Datapoints]") { static_assert(std::is_same_v<std::remove_cv_t<decltype(test.TypeAccess)>, Helper::READWRITE>); }
+    SECTION("datapoint type")
+    {
+        const auto value = test.get();
+        constexpr Temperature expected{};
+        static_assert(std::is_same_v<decltype(value), decltype(expected)>);
+    }
 
-TEST_CASE("Test datapoint read / write", "[Datapoints]")
-{
-    const auto initialValue = test.get();
-    constexpr auto EXPECTED = 123.0;
-    REQUIRE((initialValue == Temperature{ 4711, EXPECTED }));
+    SECTION("datapoint access")
+    {
+        static_assert(std::is_same_v<std::remove_cv_t<decltype(test.TypeAccess)>, Helper::READWRITE>);
+    }
 
-    // write new data
-    constexpr float newValue{ 123.4f };
-    constexpr uint32_t newRaw{ 1234u };
-    const Temperature newData{ newRaw, newValue };
-    test.set(newData);
+    SECTION("datapoint read / write")
+    {
+        const auto initialValue = test.get();
+        constexpr auto EXPECTED = 123.0;
+        REQUIRE((initialValue == Temperature{ 4711, EXPECTED }));
 
-    // verify data
-    const auto newlyWrittenValue = test.get();
-    REQUIRE((newlyWrittenValue == Temperature{ newRaw, newValue }));
-}
+        // write new data
+        constexpr float newValue{ 123.4F };
+        constexpr uint32_t newRaw{ 1234U };
+        const Temperature newData{ newRaw, newValue };
+        test.set(newData);
 
-TEST_CASE("Test datapoint name", "[Datapoints]")
-{
-    using namespace std::string_literals;
-    REQUIRE(test.name == "test"s);
-}
+        // verify data
+        const auto newlyWrittenValue = test.get();
+        REQUIRE((newlyWrittenValue == Temperature{ newRaw, newValue }));
+    }
 
-TEST_CASE("Test datapoint set via group", "[Datapoints]")
-{
-    const auto initialValue = test.get();
-    constexpr auto dpId = test.getId();
-    Temperature value{};
-    REQUIRE(DefaultGroup.getDatapoint(dpId, value));
-    REQUIRE(value.raw == initialValue.raw);
-    REQUIRE(test().raw == initialValue.raw);
-    REQUIRE_THAT(test().value, Catch::Matchers::WithinRel(static_cast<double>(value.value), EPSILON));
+    SECTION("datapoint name")
+    {
+        using namespace std::string_literals;
+        REQUIRE(test.name == "test"s);
+    }
 
-    // write new data
-    constexpr float newValue{ 321.4f };
-    constexpr uint32_t newRaw{ 3214u };
-    const Temperature newData{ newRaw, newValue };
+    SECTION("datapoint set via group")
+    {
+        const auto initialValue = test.get();
+        constexpr auto dpId = test.getId();
+        Temperature value{};
+        REQUIRE(DefaultGroup.getDatapoint(dpId, value));
+        REQUIRE(value.raw == initialValue.raw);
+        REQUIRE(test().raw == initialValue.raw);
+        REQUIRE_THAT(test().value, Catch::Matchers::WithinRel(static_cast<double>(value.value), EPSILON));
 
-    REQUIRE(DefaultGroup.setDatapoint(dpId, newData));
-    REQUIRE((test() == Temperature{ newRaw, newValue }));
-}
+        // write new data
+        constexpr float newValue{ 321.4F };
+        constexpr uint32_t newRaw{ 3214U };
+        const Temperature newData{ newRaw, newValue };
 
-TEST_CASE("Test datapoint 'testWithoutDefaultValue' default values via get() method", "[Datapoints]") { REQUIRE((testWithoutDefaultValue.get() == Temperature{})); }
+        REQUIRE(DefaultGroup.setDatapoint(dpId, newData));
+        REQUIRE((test() == Temperature{ newRaw, newValue }));
+    }
 
-TEST_CASE("Test datapoint 'testWithoutDefaultValue' default values with () operator", "[Datapoints]") { REQUIRE((testWithoutDefaultValue() == Temperature{})); }
+    SECTION("datapoint 'testWithoutDefaultValue' default values via get() method")
+    {
+        REQUIRE((testWithoutDefaultValue.get() == Temperature{}));
+    }
 
-TEST_CASE("Test 'testWithoutDefaultValue' version definition", "[Datapoints]")
-{
-    constexpr auto version = testWithoutDefaultValue.getVersion();
-    REQUIRE(version.major == 2);
-    REQUIRE(version.minor == 10);
-    REQUIRE(version.build == 223);
-}
+    SECTION("datapoint 'testWithoutDefaultValue' default values with () operator")
+    {
+        REQUIRE((testWithoutDefaultValue() == Temperature{}));
+    }
 
-TEST_CASE("Test datapoint 'testWithoutDefaultValue' access", "[Datapoints]")
-{
-    static_assert(std::is_same_v<std::remove_cv_t<decltype(testWithoutDefaultValue.TypeAccess)>, Helper::READONLY>);
-}
+    SECTION("'testWithoutDefaultValue' version definition")
+    {
+        constexpr auto version = testWithoutDefaultValue.getVersion();
+        REQUIRE(version.major == 2);
+        REQUIRE(version.minor == 10);
+        REQUIRE(version.build == 223);
+    }
 
-TEST_CASE("Test 'testWithoutDefaultValueWriteOnly' version definition", "[Datapoints]")
-{
-    constexpr auto version = testWithoutDefaultValueWriteOnly.getVersion();
-    REQUIRE(version.major == 0);
-    REQUIRE(version.minor == 2);
-    REQUIRE(version.build == 3);
-}
+    SECTION("datapoint 'testWithoutDefaultValue' access")
+    {
+        static_assert(std::is_same_v<std::remove_cv_t<decltype(testWithoutDefaultValue.TypeAccess)>, Helper::READONLY>);
+    }
 
-TEST_CASE("Test datapoint 'testWithoutDefaultValueWriteOnly' access", "[Datapoints]")
-{
-    static_assert(std::is_same_v<std::remove_cv_t<decltype(testWithoutDefaultValueWriteOnly.TypeAccess)>, Helper::WRITEONLY>);
-}
+    SECTION("'testWithoutDefaultValueWriteOnly' version definition")
+    {
+        constexpr auto version = testWithoutDefaultValueWriteOnly.getVersion();
+        REQUIRE(version.major == 0);
+        REQUIRE(version.minor == 2);
+        REQUIRE(version.build == 3);
+    }
 
-TEST_CASE("Test datapoint 'testWithoutDefaultValueWriteOnly' can't read", "[Datapoints]")
-{
-    constexpr auto dpId = testWithoutDefaultValueWriteOnly.getId();
-    Temperature value{};
-    REQUIRE(!DefaultGroup.getDatapoint(dpId, value));
-}
+    SECTION("datapoint 'testWithoutDefaultValueWriteOnly' access")
+    {
+        static_assert(std::is_same_v<std::remove_cv_t<decltype(testWithoutDefaultValueWriteOnly.TypeAccess)>, Helper::WRITEONLY>);
+    }
 
-TEST_CASE("Test datapoint 'testWithoutDefaultValue' set via group", "[Datapoints]")
-{
-    const auto initialValue = testWithoutDefaultValue.get();
-    constexpr auto dpId = testWithoutDefaultValue.getId();
-    Temperature value{};
-    REQUIRE(DefaultGroup.getDatapoint(dpId, value));
-    REQUIRE((value == initialValue));
+    SECTION("datapoint 'testWithoutDefaultValueWriteOnly' can't read")
+    {
+        constexpr auto dpId = testWithoutDefaultValueWriteOnly.getId();
+        Temperature value{};
+        REQUIRE(!DefaultGroup.getDatapoint(dpId, value));
+    }
 
-    // write new data
-    constexpr float newValue{ 321.4f };
-    constexpr uint32_t newRaw{ 3214u };
-    const Temperature newData{ newRaw, newValue };
+    SECTION("datapoint 'testWithoutDefaultValue' set via group")
+    {
+        const auto initialValue = testWithoutDefaultValue.get();
+        constexpr auto dpId = testWithoutDefaultValue.getId();
+        Temperature value{};
+        REQUIRE(DefaultGroup.getDatapoint(dpId, value));
+        REQUIRE((value == initialValue));
 
-    REQUIRE(!DefaultGroup.setDatapoint(dpId, newData));
-}
+        // write new data
+        constexpr float newValue{ 321.4F };
+        constexpr uint32_t newRaw{ 3214U };
+        const Temperature newData{ newRaw, newValue };
 
-TEST_CASE("Test datapoint 'testWithoutDefaultValue' access, write anyway", "[Datapoints]")
-{
-    constexpr Temperature testValue{ .raw = 444444, .value = 12345.0 };
-    const auto val = testWithoutDefaultValue.get();
-    static_assert(std::is_same_v<std::remove_cv_t<decltype(testWithoutDefaultValue.TypeAccess)>, Helper::READONLY>);
-    REQUIRE(testWithoutDefaultValue.get().raw != testValue.raw);
-    testWithoutDefaultValue = testValue;
-    REQUIRE((testWithoutDefaultValue.get() == testValue));
-    testWithoutDefaultValue = val;
-}
+        REQUIRE(!DefaultGroup.setDatapoint(dpId, newData));
+    }
 
-TEST_CASE("Test datapoint 'testWithoutDefaultValueWriteOnly' access, read anyway", "[Datapoints]")
-{
-    static_assert(std::is_same_v<std::remove_cv_t<decltype(testWithoutDefaultValueWriteOnly.TypeAccess)>, Helper::WRITEONLY>);
-    const auto val = testWithoutDefaultValueWriteOnly();
-    REQUIRE((val == Temperature{}));
-}
+    SECTION("datapoint 'testWithoutDefaultValue' access, write anyway")
+    {
+        constexpr Temperature testValue{ .raw = 444444, .value = 12345.0 };
+        static_assert(std::is_same_v<std::remove_cv_t<decltype(testWithoutDefaultValue.TypeAccess)>, Helper::READONLY>);
+        REQUIRE(testWithoutDefaultValue.get().raw != testValue.raw);
+        testWithoutDefaultValue = testValue;
+        REQUIRE((testWithoutDefaultValue.get() == testValue));
+    }
 
-TEST_CASE("Test datapoint dispatcher get() existing", "[Datapoints]")
-{
-    Temperature temperatureTest{ .raw = 1234, .value = 11111.f };
-    REQUIRE(Dispatcher.getDatapoint(testWithoutDefaultValue.getId(), temperatureTest));
-    REQUIRE((testWithoutDefaultValue.get() == temperatureTest));
-}
+    SECTION("datapoint 'testWithoutDefaultValueWriteOnly' access, read anyway")
+    {
+        static_assert(std::is_same_v<std::remove_cv_t<decltype(testWithoutDefaultValueWriteOnly.TypeAccess)>, Helper::WRITEONLY>);
+        const auto val = testWithoutDefaultValueWriteOnly();
+        REQUIRE((val == Temperature{}));
+    }
 
-TEST_CASE("Test datapoint dispatcher get() not existing", "[Datapoints]")
-{
-    constexpr uint32_t dummyId = 42;
-    Temperature temperatureTest{ .raw = 1234, .value = 11111.f };
-    REQUIRE(!Dispatcher.getDatapoint(dummyId, temperatureTest));
-}
+    SECTION("datapoint dispatcher get() existing")
+    {
+        Temperature temperatureTest{ .raw = 1234, .value = 11111.F };
+        REQUIRE(Dispatcher.getDatapoint(testWithoutDefaultValue.getId(), temperatureTest));
+        REQUIRE((testWithoutDefaultValue.get() == temperatureTest));
+    }
 
-TEST_CASE("Test datapoint dispatcher setDatapoint() not existing", "[Datapoints]")
-{
-    constexpr uint32_t dummyId = 42;
-    constexpr Temperature temperatureTest{ .raw = 1234, .value = 11111.f };
-    REQUIRE(!Dispatcher.setDatapoint(dummyId, temperatureTest));
-}
+    SECTION("datapoint dispatcher get() not existing")
+    {
+        constexpr uint32_t dummyId = 42;
+        Temperature temperatureTest{ .raw = 1234, .value = 11111.F };
+        REQUIRE(!Dispatcher.getDatapoint(dummyId, temperatureTest));
+    }
 
-TEST_CASE("Test datapoint dispatcher setDatapoint() existing with READONLY", "[Datapoints]")
-{
-    constexpr Temperature temperatureTest{ .raw = 1234, .value = 11111.f };
-    REQUIRE(!Dispatcher.setDatapoint(testWithoutDefaultValue.getId(), temperatureTest));
-}
+    SECTION("datapoint dispatcher setDatapoint() not existing")
+    {
+        constexpr uint32_t dummyId = 42;
+        constexpr Temperature temperatureTest{ .raw = 1234, .value = 11111.F };
+        REQUIRE(!Dispatcher.setDatapoint(dummyId, temperatureTest));
+    }
 
-TEST_CASE("Test datapoint dispatcher setDatapoint() existing with WRITEONLY", "[Datapoints]")
-{
-    const auto initialValue = testWithoutDefaultValueWriteOnly();
-    constexpr Temperature temperatureTest{ .raw = 1234, .value = 11111.f };
-    Temperature readValue{};
-    REQUIRE(!Dispatcher.getDatapoint(testWithoutDefaultValueWriteOnly.getId(), readValue));
-    REQUIRE(Dispatcher.setDatapoint(testWithoutDefaultValueWriteOnly.getId(), temperatureTest));
-    REQUIRE((testWithoutDefaultValueWriteOnly() == temperatureTest));
-    testWithoutDefaultValueWriteOnly = initialValue;
-}
+    SECTION("datapoint dispatcher setDatapoint() existing with READONLY")
+    {
+        constexpr Temperature temperatureTest{ .raw = 1234, .value = 11111.F };
+        REQUIRE(!Dispatcher.setDatapoint(testWithoutDefaultValue.getId(), temperatureTest));
+    }
 
-TEST_CASE("Test datapoint dispatcher setDatapoint() and getDatapoint() existing", "[Datapoints]")
-{
-    const auto initialValue = test();
-    constexpr Temperature temperatureTest{ .raw = 1234, .value = 11111.f };
-    Temperature readValue{};
-    REQUIRE(Dispatcher.getDatapoint(test.getId(), readValue));
-    REQUIRE(Dispatcher.setDatapoint(test.getId(), temperatureTest));
+    SECTION("datapoint dispatcher setDatapoint() existing with WRITEONLY")
+    {
+        constexpr Temperature temperatureTest{ .raw = 1234, .value = 11111.F };
+        Temperature readValue{};
+        REQUIRE(!Dispatcher.getDatapoint(testWithoutDefaultValueWriteOnly.getId(), readValue));
+        REQUIRE(Dispatcher.setDatapoint(testWithoutDefaultValueWriteOnly.getId(), temperatureTest));
+        REQUIRE((testWithoutDefaultValueWriteOnly() == temperatureTest));
+    }
 
-    REQUIRE((test() == temperatureTest));
+    SECTION("datapoint dispatcher setDatapoint() and getDatapoint() existing")
+    {
+        constexpr Temperature temperatureTest{ .raw = 1234, .value = 11111.F };
+        Temperature readValue{};
+        REQUIRE(Dispatcher.getDatapoint(test.getId(), readValue));
+        REQUIRE(Dispatcher.setDatapoint(test.getId(), temperatureTest));
+        REQUIRE((test() == temperatureTest));
+    }
 
-    test = initialValue;
+    test = initalTestValue;
+    testWithoutDefaultValue = initalTestWithoutDefaultValueValue;
+    testWithoutDefaultValueWriteOnly = initalTestWithoutDefaultValueWriteOnlyValue;
 }
--- test/catch2/groups/test.cpp
+++ test/catch2/groups/test.cpp
@@ -2,56 +2,77 @@
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
 #include <datalayer.h>
 
-TEST_CASE("Test default group name", "[Groups]")
+TEST_CASE("Test groups", "[Groups]")
 {
-    using namespace std::string_literals;
-    REQUIRE(DefaultGroupInfo.name == "DefaultGroup"s);
+    SECTION("default group name")
+    {
+        using namespace std::string_literals;
+        REQUIRE(DefaultGroupInfo.name == "DefaultGroup"s);
+    }
+
+    SECTION("default group id")
+    {
+        REQUIRE(DefaultGroupInfo.baseId == 0x4000);
+    }
+
+    SECTION("default group version")
+    {
+        constexpr auto version = DefaultGroupInfo.version;
+        REQUIRE(version.major == 1);
+        REQUIRE(version.minor == 2);
+        REQUIRE(version.build == 3);
+    }
+
+    SECTION("default group persistence")
+    {
+        REQUIRE(DefaultGroupInfo.persist == DataLayer::Persistance::None);
+    }
+
+    SECTION("cyclic group name")
+    {
+        using namespace std::string_literals;
+        REQUIRE(DefaultCyclicGroupInfo.name == "DefaultCyclicGroup"s);
+    }
+
+    SECTION("cyclic group id", "[Groups]")
+    {
+        REQUIRE(DefaultCyclicGroupInfo.baseId == 0x5000);
+    }
+
+    SECTION("cyclic group version")
+    {
+        constexpr auto version = DefaultCyclicGroupInfo.version;
+        REQUIRE(version.major == 4);
+        REQUIRE(version.minor == 5);
+        REQUIRE(version.build == 6);
+    }
+
+    SECTION("cyclic group persistence")
+    {
+        REQUIRE(DefaultCyclicGroupInfo.persist == DataLayer::Persistance::Cyclic);
+    }
+
+    SECTION("on-write group name")
+    {
+        using namespace std::string_literals;
+        REQUIRE(DefaultOnWriteGroupInfo.name == "DefaultOnWriteGroup"s);
+    }
+
+    SECTION("on-write group id")
+    {
+        REQUIRE(DefaultOnWriteGroupInfo.baseId == 0x6000);
+    }
+
+    SECTION("on-write group version")
+    {
+        constexpr auto version = DefaultOnWriteGroupInfo.version;
+        REQUIRE(version.major == 7);
+        REQUIRE(version.minor == 8);
+        REQUIRE(version.build == 9);
+    }
+
+    SECTION("on-write group persistence")
+    {
+        REQUIRE(DefaultOnWriteGroupInfo.persist == DataLayer::Persistance::OnWrite);
+    }
 }
-
-TEST_CASE("Test default group id", "[Groups]") { REQUIRE(DefaultGroupInfo.baseId == 0x4000); }
-
-TEST_CASE("Test default group version", "[Groups]")
-{
-    constexpr auto version = DefaultGroupInfo.version;
-    REQUIRE(version.major == 1);
-    REQUIRE(version.minor == 2);
-    REQUIRE(version.build == 3);
-}
-
-TEST_CASE("Test default group persistence", "[Groups]") { REQUIRE(DefaultGroupInfo.persist == DataLayer::Persistance::None); }
-
-TEST_CASE("Test cyclic group name", "[Groups]")
-{
-    using namespace std::string_literals;
-    REQUIRE(DefaultCyclicGroupInfo.name == "DefaultCyclicGroup"s);
-}
-
-TEST_CASE("Test cyclic group id", "[Groups]") { REQUIRE(DefaultCyclicGroupInfo.baseId == 0x5000); }
-
-TEST_CASE("Test cyclic group version", "[Groups]")
-{
-    constexpr auto version = DefaultCyclicGroupInfo.version;
-    REQUIRE(version.major == 4);
-    REQUIRE(version.minor == 5);
-    REQUIRE(version.build == 6);
-}
-
-TEST_CASE("Test cyclic group persistence", "[Groups]") { REQUIRE(DefaultCyclicGroupInfo.persist == DataLayer::Persistance::Cyclic); }
-
-TEST_CASE("Test on-write group name", "[Groups]")
-{
-    using namespace std::string_literals;
-    REQUIRE(DefaultOnWriteGroupInfo.name == "DefaultOnWriteGroup"s);
-}
-
-TEST_CASE("Test on-write group id", "[Groups]") { REQUIRE(DefaultOnWriteGroupInfo.baseId == 0x6000); }
-
-TEST_CASE("Test on-write group version", "[Groups]")
-{
-    constexpr auto version = DefaultOnWriteGroupInfo.version;
-    REQUIRE(version.major == 7);
-    REQUIRE(version.minor == 8);
-    REQUIRE(version.build == 9);
-}
-
-TEST_CASE("Test on-write group persistence", "[Groups]") { REQUIRE(DefaultOnWriteGroupInfo.persist == DataLayer::Persistance::OnWrite); }
